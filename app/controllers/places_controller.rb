d8e8e7542416fa7de20be54979f104b84d84f1bf
ericeschliman <eric.eschliman@kellogg.northwestern.edu>
2023-02-21 15:00:12 +0000

    final submission

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,7 +1,23 @@
 class PlacesController < ApplicationController
 
-def index
-    @places = ["Australia", "Amsterdam", "London", "Interlachen", "Carmel"]
-end
+    def index
+        @places = Place.all
+    end
+
+    def show
+        @place = Place.find_by({"id" => params["id"]})
+        @posts = Post.where({"place_id" => @place["id"] })
+    end
+
+    def new
+        @place = Place.new
+    end
+
+    def create
+       @place = Place.new
+       @place["name"] = params["place"]["name"]
+       @place.save
+       redirect_to "/places"
+    end
 
 end
